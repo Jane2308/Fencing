a5c49cf6c83444985ba54b6e9ff099db256c8e4f
Jane2308 <janeenattis@hotmail.nl>
2018-02-08 21:01:17 +0100

    Add files via upload

---
+++ Crazy_stories.py
@@ -0,0 +1,16 @@
+loop = 1
+
+while (loop < 10):
+    print("Welcome to your crazy story")
+
+    person = input("Enter a name")
+    verb = input("Enter an 'ing' verb")
+    adjective = input("Enter an adjective")
+    noun = input("Enter a country or state")
+
+    print ("Computer Science students at Rice University created ",person, " Script, a programming language inspired by ",person,". " )
+    print ( "They said that ",person,"Script was a “programming language ",person," would approve of. ")
+    print ("Just like he/she is ",verb, noun," great again, we hope our efforts will make programming great again.")
+    print ("In the spirit of ",person,", the language only allowed the use of numbers over 1,000,000. " )
+    print ("It had no import statements (All code has to be home-grown and ",noun," made.). " )
+    print ("And it required that all ",adjective," programs end with the statement ",noun," is great.")
